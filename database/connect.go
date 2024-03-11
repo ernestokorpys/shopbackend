f760a5d26cb7f442493997bf9965e9e5ad34aeb1
ernesto.korpys <ernesto.korpys@gmail.com>
2024-03-10 22:00:12 -0300

    .env try 4

--- database/connect.go
+++ database/connect.go
@@ -7,17 +7,11 @@ import (
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectDB() *mongo.Client {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	uri := os.Getenv("MONGODB_URI")
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/ernestokorpys/shopbackend/database"
@@ -9,17 +8,14 @@ import (
 	"github.com/ernestokorpys/shopbackend/routes"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	client := database.ConnectDB() //cliente de mongo.DB
-	// Colección y contexto
-	// collection := client.Database("onlineshop").Collection("shopUsers")
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error")
+	// }
 	port := os.Getenv("PORT")
 	app := fiber.New()
 	// Middleware de recuperación de errores
