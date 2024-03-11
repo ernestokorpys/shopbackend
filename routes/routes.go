0f1444f9bae97294dbe30b791a3dc8b21b8f886e
ernesto.korpys <ernesto.korpys@gmail.com>
2024-03-11 20:22:36 -0300

    test1

--- routes/routes.go
+++ routes/routes.go
@@ -2,14 +2,13 @@ package routes
 
 import (
 	"github.com/ernestokorpys/shopbackend/controller"
-	"github.com/ernestokorpys/shopbackend/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.RegisterUser)
 	app.Post("/api/login", controller.Login)
-	app.Use(middleware.IsAuthenticate)
+	// app.Use(middleware.IsAuthenticate)
 	app.Post("/api/addproduct", controller.AddProduct)
 	app.Get("/api/products", controller.GetProducts)
 
