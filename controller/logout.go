a812c9bb741a5227124754dd6f4d7ea3afd3ea8d
ernesto.korpys <ernesto.korpys@gmail.com>
2024-03-12 20:33:14 -0300

    logout changes

--- controller/logout.go
+++ controller/logout.go
@@ -1,11 +1,20 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Logout(c *fiber.Ctx) error {
 	// Remover la cookie del token JWT del cliente
-	c.ClearCookie("jwt")
-
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie) // Agregar la cookie a la
 	// Puedes enviar una respuesta indicando que el logout fue exitoso
 	return c.JSON(fiber.Map{
 		"message": "Logout successful",
