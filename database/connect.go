654066e68264cae9906671f3b8f8f026185fdc45
ernesto.korpys <ernesto.korpys@gmail.com>
2024-03-12 12:08:52 -0300

    test 4

--- database/connect.go
+++ database/connect.go
@@ -7,15 +7,16 @@ import (
 	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectDB() *mongo.Client {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	uri := os.Getenv("MONGODB_URI")
 
--- routes/routes.go
+++ routes/routes.go
@@ -2,23 +2,14 @@ package routes
 
 import (
 	"github.com/ernestokorpys/shopbackend/controller"
+	"github.com/ernestokorpys/shopbackend/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.RegisterUser)
 	app.Post("/api/login", controller.Login)
-	// app.Use(middleware.IsAuthenticate)
+	app.Use(middleware.IsAuthenticate)
 	app.Post("/api/addproduct", controller.AddProduct)
 	app.Get("/api/products", controller.GetProducts)
-
-	// app.Post("/api/post", controller.CreatePost)
-	// app.Get("/api/allpost", controller.AllPost)
-	// app.Get("/api/allpost/:id", controller.DetailPost)
-	// app.Put("/api/updatepost/:id", controller.UpdatePost)
-	// app.Get("/api/uniquepost", controller.UniquePost)
-	// app.Delete("/api/deletepost/:id", controller.DeletePost)
-	// app.Post("/api/upload-image", controller.UploadImage)
-	// app.Static("/api/uploads", "./uploads")
-
 }
