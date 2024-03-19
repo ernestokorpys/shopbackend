a4cb4e599819a2992a1584304c355b9464986089
ernesto.korpys <ernesto.korpys@gmail.com>
2024-03-19 20:37:56 -0300

    allow web test from vercel

--- main.go
+++ main.go
@@ -31,10 +31,10 @@ func main() {
 
 	// Configuración de CORS
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173", // Permite solicitudes desde este origen
-		AllowMethods:     "GET,POST,PUT,DELETE",   // Permite estos métodos HTTP
-		AllowCredentials: true,                    // Permite credenciales (cookies)
-		AllowHeaders:     "Content-Type",          // Permitir los encabezados necesarios
+		AllowOrigins:     "http://localhost:5173,https://frontendreact-neon.vercel.app",
+		AllowMethods:     "GET,POST,PUT,DELETE", // Permite estos métodos HTTP
+		AllowCredentials: true,                  // Permite credenciales (cookies)
+		AllowHeaders:     "Content-Type",        // Permitir los encabezados necesarios
 	}))
 
 	routes.Setup(app)      //maneja las solicitudes entrantes
